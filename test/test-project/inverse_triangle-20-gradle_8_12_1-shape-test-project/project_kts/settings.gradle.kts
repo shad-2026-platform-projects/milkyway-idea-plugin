875205532ad1e2ce0c7dea88816d907e6f1b8d4d
lelikut <37458669+lelikut@users.noreply.github.com>
2026-04-13 02:38:47 +0300

    chore: add configuration example for testing the plugin on another plugin
    Usage: ./gradlew printDependencies

--- test/test-project/inverse_triangle-20-gradle_8_12_1-shape-test-project/project_kts/settings.gradle.kts
+++ test/test-project/inverse_triangle-20-gradle_8_12_1-shape-test-project/project_kts/settings.gradle.kts
@@ -5,6 +5,13 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    includeBuild("../../../../milkyway-plugin")
+
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
 }
 plugins {
     id("com.fueledbycaffeine.spotlight") version "1.4.1"
@@ -17,25 +24,25 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
-} 
-include (":core:push")
-include (":feature:contact")
-include (":feature:login")
-include (":domain:identity")
-include (":domain:checkout")
-include (":domain:cart")
-include (":repository:user")
-include (":repository:profile")
-include (":repository:feed")
-include (":repository:comment")
-include (":repository:post")
-include (":model:search")
-include (":model:location")
-include (":model:network")
-include (":model:sync")
-include (":model:share")
-include (":model:group")
-include (":model:message")
-include (":model:notification")
-include (":model:setting")
-include (":app:app")
+}
+include(":core:push")
+include(":feature:contact")
+include(":feature:login")
+include(":domain:identity")
+include(":domain:checkout")
+include(":domain:cart")
+include(":repository:user")
+include(":repository:profile")
+include(":repository:feed")
+include(":repository:comment")
+include(":repository:post")
+include(":model:search")
+include(":model:location")
+include(":model:network")
+include(":model:sync")
+include(":model:share")
+include(":model:group")
+include(":model:message")
+include(":model:notification")
+include(":model:setting")
+include(":app:app")
